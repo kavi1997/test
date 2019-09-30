c52afc01ee74d39f97cbc135cd84a44015d887b6
Kavitha R <Kavitha R@LAPTOP-KAVI>
2019-09-30 09:40:51 +0530

    First Commit

---
+++ test/src/test/java/com/maven/test/countATest.java
@@ -0,0 +1,16 @@
+package com.maven.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class countATest {
+
+	@Test
+	public void test() {
+		JunitTesting test = new JunitTesting();
+		int output= test.countA("alphabet");
+		assertEquals(2,output);
+	}
+
+}
